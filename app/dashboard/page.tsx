8be9b269466035a6c13055ff92f8b7621062d831
huangyun <hy020316@126.com>
2024-10-30 01:30:21 +0800

    修复

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from 'react'
 import ArticleList from './ArticleList'
 import { fetchArticles } from './actions'
 
+export const dynamic = 'force-dynamic'
+
 export default async function DashboardPage() {
   const articles = await fetchArticles()
 
